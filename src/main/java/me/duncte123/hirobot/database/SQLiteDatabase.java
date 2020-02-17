75eec135634dad411b53620fdcc33c953c402be9
Duncan Sterken <duncan.sterken123@gmail.com>
2020-02-17 12:31:26 +0100

    Pls don't crash anymore

--- src/main/java/me/duncte123/hirobot/database/SQLiteDatabase.java
+++ src/main/java/me/duncte123/hirobot/database/SQLiteDatabase.java
@@ -7,10 +7,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class SQLiteDatabase implements Database {
     private static final Logger LOGGER = LoggerFactory.getLogger(SQLiteDatabase.class);
@@ -38,17 +36,21 @@ public class SQLiteDatabase implements Database {
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.setIdleTimeout(TimeUnit.SECONDS.toMillis(30));
+        config.setLeakDetectionThreshold(60 * 1000);
         ds = new HikariDataSource(config);
 
-        try (final Statement statement = ds.getConnection().createStatement()) {
-            // language=SQLite
-            statement.execute("CREATE TABLE IF NOT EXISTS valentines (" +
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    "user_id VARCHAR(20) NOT NULL," +
-                    "buddy_index int(2) NOT NULL DEFAULT -1" +
-                    ");");
+        try (final Connection connection = ds.getConnection()) {
+            try (final Statement statement = connection.createStatement()) {
+                // language=SQLite
+                statement.execute("CREATE TABLE IF NOT EXISTS valentines (" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "user_id VARCHAR(20) NOT NULL," +
+                        "buddy_index int(2) NOT NULL DEFAULT -1" +
+                        ");");
 
-            LOGGER.info("Table initialised");
+                LOGGER.info("Table initialised");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -56,14 +58,16 @@ public class SQLiteDatabase implements Database {
 
     @Override
     public void setValentine(long userId, int buddyIndex) {
-        try (final PreparedStatement preparedStatement = ds.getConnection()
-                // language=SQLite
-                .prepareStatement("INSERT INTO valentines(buddy_index, user_id) VALUES(? , ?)")) {
+        try (final Connection connection = ds.getConnection()) {
+            try (final PreparedStatement preparedStatement =
+                         // language=SQLite
+                         connection.prepareStatement("INSERT INTO valentines(buddy_index, user_id) VALUES(? , ?)")) {
 
-            preparedStatement.setInt(1, buddyIndex);
-            preparedStatement.setString(2, String.valueOf(userId));
+                preparedStatement.setInt(1, buddyIndex);
+                preparedStatement.setString(2, String.valueOf(userId));
 
-            preparedStatement.executeUpdate();
+                preparedStatement.executeUpdate();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -71,15 +75,17 @@ public class SQLiteDatabase implements Database {
 
     @Override
     public int getValentine(long userId) {
-        try (final PreparedStatement preparedStatement = ds.getConnection()
-                // language=SQLite
-                .prepareStatement("SELECT buddy_index FROM valentines WHERE user_id = ?")) {
+        try (final Connection connection = ds.getConnection()) {
+            try (final PreparedStatement preparedStatement =
+                         // language=SQLite
+                         connection.prepareStatement("SELECT buddy_index FROM valentines WHERE user_id = ?")) {
 
-            preparedStatement.setString(1, String.valueOf(userId));
+                preparedStatement.setString(1, String.valueOf(userId));
 
-            try (final ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return resultSet.getInt("buddy_index");
+                try (final ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return resultSet.getInt("buddy_index");
+                    }
                 }
             }
         } catch (SQLException e) {
