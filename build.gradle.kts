21122aebcc0f2cd021de605a83f5143acf79828f
duncte123 <contact@duncte123.me>
2020-07-29 10:50:11 +0200

    Update JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ repositories {
 }
 
 dependencies {
-    implementation(group = "net.dv8tion", name = "JDA", version = "4.1.1_165")
+    implementation(group = "net.dv8tion", name = "JDA", version = "4.2.0_182")
     implementation(group = "com.jagrosh", name = "jda-utilities-command", version = "3.0.3")
     
     implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.10.1")
